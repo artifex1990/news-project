3993610464aca4c28defb6ed375443aebf860aa4
pavel demyanov <pavel@demjanov.ru>
2025-05-14 00:28:14 +0300

    refactor local get news, categories method

---
+++ src/constants/constants.js
@@ -0,0 +1,5 @@
+export const BASE_URL_LOCAL = import.meta.env.VITE_NEWS_BASE_API_LOCAL_URL;
+export const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL;
+export const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
+export const PAGE_SIZE = 10;
+export const TOTAL_PAGES = 10;
