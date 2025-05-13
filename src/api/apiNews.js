6120185b21bcc551f2084f1ae433c8df0b6476e3
pavel demyanov <pavel@demjanov.ru>
2025-05-14 00:21:02 +0300

    mini refactor api use local when limit 20 requests per day

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -1,4 +1,6 @@
-import axios from "axios";
+import { getLocalCategories } from "../helpers/local/getLocalCategories";
+import { getLocalNews } from "../helpers/local/getLocalNews";
+import { getContent } from "./getContent";
 
 const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL;
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
@@ -10,52 +12,26 @@ export const getNews = async ({
   keywords,
 }) => {
   try {
-    const response = await axios.get(`${BASE_URL}search.json`, {
-      params: {
-        apiKey: API_KEY,
-        page_number: page_number,
-        page_size: page_size,
-        category,
-        keywords,
-      },
+    return await getContent(`${BASE_URL}search`, {
+      apiKey: API_KEY,
+      page_number,
+      page_size,
+      category,
+      keywords,
     });
-
-    const pageNavigation = page_number * page_size - 10;
-
-    response.data.news = response.data.news.filter(
-      (n) => !category || n.category.indexOf(category) !== -1
-    );
-
-    if (keywords) {
-      response.data.news = response.data.news.filter(
-        (n) =>
-          n.title.indexOf(keywords) !== -1 ||
-          n.description.indexOf(keywords) !== -1
-      );
-    }
-
-    response.data.news = response.data.news.slice(
-      pageNavigation,
-      pageNavigation + page_size
-    );
-    response.page = page_number;
-
-    return response.data;
   } catch (error) {
-    throw Error("Error fetching news: " + error);
+    console.log(error);
+    return await getLocalNews(page_number, page_size, category, keywords);
   }
 };
 
 export const getCategories = async () => {
   try {
-    const response = await axios.get(`${BASE_URL}categories.json`, {
-      params: {
-        apiKey: API_KEY,
-      },
+    return await getContent(`${BASE_URL}available/categories`, {
+      apiKey: API_KEY,
     });
-
-    return response.data;
   } catch (error) {
-    throw Error("Error fetching news: " + error);
+    console.log(error);
+    return await getLocalCategories();
   }
 };
